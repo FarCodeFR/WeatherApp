a07368e2368881b9d789dd0f66d8439ddd311bcb
FarCodeFR <Timo-44@hotmail.com>
2026-02-17 08:30:59 +0100

    Correction du jour et de la nuit et ajout de commentaire

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,31 @@
 import Heros from "@/components/Heros";
 import Loader from "@/components/Loader";
 import ScrollSheet from "@/components/ScrollSheet";
+import { useLocationCity } from "@/hooks/useLocationCity";
+import { computeIsDay, formatHour } from "@/services/forecast-utils";
 import { getWeatherInterpretation } from "@/services/meteo-service";
 import { ImageBackground, StyleSheet, View } from "react-native";
-import { useLocationCity } from "@/hooks/useLocationCity";
 
 export default function Index() {
   const { weather, city, searchCity } = useLocationCity();
-
   const currentWeather = weather?.current_weather;
+  const apiDay = currentWeather?.is_day;
+
+  const sunriseISO = weather?.daily?.sunrise?.[0];
+  const sunsetISO = weather?.daily?.sunset?.[0];
+  const nowISO = weather?.current_weather?.time;
+
+  const lever = sunriseISO ? formatHour(sunriseISO) : undefined;
+  const coucher = sunsetISO ? formatHour(sunsetISO) : undefined;
+
+  const isDaySafe =
+    sunriseISO && sunsetISO && nowISO
+      ? computeIsDay(nowISO, sunriseISO, sunsetISO)
+      : Number(apiDay) === 1;
 
   const weatherClimat = currentWeather
-    ? getWeatherInterpretation(
-        currentWeather.weather_code,
-        currentWeather.is_day,
-      )
-    : getWeatherInterpretation(0, 1);
-
-  const lever = weather?.daily?.sunrise?.[0]?.split("T")?.[1];
-  const coucher = weather?.daily?.sunset?.[0]?.split("T")?.[1];
+    ? getWeatherInterpretation(currentWeather.weather_code, isDaySafe)
+    : getWeatherInterpretation(0, true);
 
   return currentWeather ? (
     <View style={styles.container}>
@@ -36,7 +43,12 @@ export default function Index() {
           coucher={coucher}
           interpretation={weatherClimat}
         />
-        <ScrollSheet weather={weather} searchCity={searchCity} />
+        <ScrollSheet
+          sunriseISO={sunriseISO}
+          sunsetISO={sunsetISO}
+          weather={weather}
+          searchCity={searchCity}
+        />
       </ImageBackground>
     </View>
   ) : (
