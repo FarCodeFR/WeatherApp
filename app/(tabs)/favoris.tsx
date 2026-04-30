d28f70a03de15c251190c71de555a117943d181e
FarCodeFR <Timo-44@hotmail.com>
2026-04-30 21:39:55 +0200

    Fix message for the favoris

--- app/(tabs)/favoris.tsx
+++ app/(tabs)/favoris.tsx
@@ -1,11 +1,13 @@
-import { BlurView } from "expo-blur";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 function Favoris() {
   return (
     <View style={styles.container}>
       <View style={styles.containerCards}>
-        <Text>Mes villes favorites</Text>
+        <Text style={styles.comingSoonText}>Prochainement</Text>
+        <Text style={styles.subtitle}>
+          Gardez vos villes préférées à portée de main !
+        </Text>
       </View>
     </View>
   );
@@ -13,17 +15,31 @@ function Favoris() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#6dbfe2",
-    height: "100%",
-    width: "100%",
+    flex: 1,
     padding: 20,
     paddingTop: 100,
   },
   containerCards: {
     backgroundColor: "white",
-    height: 150,
+    height: 180,
     borderRadius: 20,
-    padding: 10,
+    padding: 15,
     width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  comingSoonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#2c3e50",
+    textAlign: "center",
+    textTransform: "uppercase",
+  },
+  subtitle: {
+    fontSize: 12,
+    color: "#666",
+    textAlign: "center",
+    marginTop: 5,
   },
 });
 export default Favoris;
